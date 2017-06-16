0f0b9925559e6280cba8d8e847f8a456d9559cba
Ian T. Nordeck <ian.nordeck@gmail.com>
2017-06-15 20:42:54 -0600

    ctreated game 2 logic - unit 8

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("Player flipped " + cardOne);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
 console.log("Player flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+ 	alert("You found a match!");
+ } else {
+ 	alert("Sorry, try again.");
+ }
+};
