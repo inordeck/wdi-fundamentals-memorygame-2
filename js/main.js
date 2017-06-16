1e18b8f827a73597ce53b3f973e15eb1ec74ac1c
Ian T. Nordeck <ian.nordeck@gmail.com>
2017-06-15 21:50:32 -0600

    created game logic - unit 11

--- js/main.js
+++ js/main.js
@@ -31,14 +31,24 @@ var checkForMatch = function() {
  }
 };
 
-var flipCard = function(cardId) {
- if (cardsInPlay.length === 2) {
- }
- console.log("Player flipped " + cards[cardId].rank);
+var flipCard = function() {
+ cardId = this.getAttribute('data-id');
+ console.log(cardId);
  cardsInPlay.push(cards[cardId].rank);
- console.log(cards[cardId].suit);
- console.log(cards[cardId].cardImage)
+ this.setAttribute("src", cards[cardId].cardImage);
+ if (cardsInPlay.length === 2) {
  checkForMatch();
+ cardsInPlay = []
+ }
 };
- flipCard(0);
- flipCard(2);
+
+var createBoard = function() {
+ for (var i = 0; i < cards.length; i++) {
+ var cardElement = document.createElement('img');
+ cardElement.setAttribute('src', 'images/back.png');
+ cardElement.setAttribute('data-id', i);
+ cardElement.addEventListener('click', flipCard);
+ document.getElementById('game-board').appendChild(cardElement);
+ }
+}
+createBoard();
